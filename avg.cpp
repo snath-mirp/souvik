1756573972130c37d66156cd40e1656945002f06
Your Name <you@example.com>
2017-06-01 07:23:35 +0530

    c++

---
+++ avg.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int N,n 
+ double avg=0;
+ 
+ cout<<"Enter the number of inputs"<<endl;
+ cin<< N;
+ 
+ for(n=0;n<N;n++)
+{cout<<"Enter a number"<<endl;
+ cin>>n
+ avg+=n;}
+ 
+cout<< "Average="<< avg;
+
+return 0;
+}
+ 
+
+
